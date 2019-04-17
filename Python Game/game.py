fd2dbfdda9170e18c2adbbf461becacb9119b711
Gabriel Mukobi <gabrielmukobi@gmail.com>
2019-04-17 15:28:45 -0700

    Refactor kinematic information into data class

--- Python Game/game.py
+++ Python Game/game.py
@@ -1,3 +1,9 @@
+"""
+game.py - Gabriel Mukobi - 2019 04 18
+A simple pygame game where the player avoids larger polygons
+to stay alive and eats smaller polygons to grow.
+"""
+
 import math
 from math import sqrt, sin, cos
 import sys
@@ -9,6 +15,13 @@ FRAMERATE = 60
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
+class Kinematics:
+    """Data class to encapsulate information about movement"""
+    def __init__(self, speed, position, friction, acceleration):
+        self.speed = speed
+        self.position = position
+        self.friction = friction
+        self.acceleration = acceleration
 
 def length(vec):
     """Get the distance between 2 2D vectors"""
@@ -16,112 +29,118 @@ def length(vec):
 
 class Actor:
     """Parent class for game actors"""
-
-    def __init__(self, speed, position, friction):
-        self.speed = speed
-        self.position = position
-        self.friction = friction
-        self.acceleration = [0, 0]
+    # TODO add rotation
+    def __init__(self, kinematics):
+        self.kinematics = kinematics
 
     def accelerate(self):
         """Accelerate the actor"""
-        return
 
     def modify_speed(self):
         """Change the actor's speed"""
-        self.speed[0] *= self.friction
-        self.speed[1] *= self.friction
-        return
+        self.kinematics.speed[0] *= self.kinematics.friction
+        self.kinematics.speed[1] *= self.kinematics.friction
 
     def move(self):
         """Move the actor based on speed and acceleration"""
         self.accelerate()
 
-        self.speed[0] += self.acceleration[0]
-        self.speed[1] += self.acceleration[1]
+        self.kinematics.speed[0] += self.kinematics.acceleration[0]
+        self.kinematics.speed[1] += self.kinematics.acceleration[1]
 
         self.modify_speed()
 
-        self.position[0] = self.position[0] + self.speed[0]
-        self.position[1] = self.position[1] + self.speed[1]
+        self.kinematics.position[0] = self.kinematics.position[0] + self.kinematics.speed[0]
+        self.kinematics.position[1] = self.kinematics.position[1] + self.kinematics.speed[1]
 
     def draw(self):
         """Draw the actor"""
-        return
 
 
 class PolygonActor(Actor):
-    def __init__(self, speed, position, friction, size, num_sides):
-        super().__init__(speed, position, friction)
+    """Class for all enemies"""
+
+    def __init__(self, kinematics, size, num_sides):
+        super().__init__(kinematics)
         self.size = size
         self.num_sides = num_sides
         self.verts = []
+        self.outline_color = [255, 255, 255]
+        self.fill_color = [0, 0, 255]
 
     def generate_poly(self):
+        """Populate the list verts with vertices of actor's polygon"""
         verts = []
         for i in range(0, self.num_sides):
             angle = 2.0 * math.pi / self.num_sides * i
-            vert_x = self.size * cos(angle) + self.position[0]
-            vert_y = self.size * sin(angle) + self.position[1]
+            vert_x = self.size * cos(angle) + self.kinematics.position[0]
+            vert_y = self.size * sin(angle) + self.kinematics.position[1]
             verts.append([vert_x, vert_y])
         self.verts = verts
 
     def draw(self):
+        """Generate polygon geometry then draws shape accordingly"""
         self.generate_poly()
-        pygame.draw.aalines(SCREEN, [255, 255, 255], True, self.verts)
-        pygame.draw.polygon(SCREEN, [255, 255, 255], self.verts)
+        pygame.draw.polygon(SCREEN, self.fill_color, self.verts)
+        pygame.draw.aalines(SCREEN, self.outline_color, True, self.verts)
 
     def collides_with(self, other_polygon_actor):
-        x_diff = self.position[0] - other_polygon_actor.position[0]
-        y_diff = self.position[1] - other_polygon_actor.position[1]
+        """Test if colliding with another polygon actor"""
+        x_diff = self.kinematics.position[0] - other_polygon_actor.kinematics.position[0]
+        y_diff = self.kinematics.position[1] - other_polygon_actor.kinematics.position[1]
         dist = sqrt(x_diff*x_diff + y_diff*y_diff)
         return dist < self.size + other_polygon_actor.size
 
 class PlayerActor(PolygonActor):
+    """Class for player character"""
+
     def accelerate(self):
         keys = pygame.key.get_pressed()
         acc_limit = 0.4
-        self.acceleration = [0, 0]
+        self.kinematics.acceleration = [0, 0]
         if keys[pygame.K_LEFT]:
-            self.acceleration[0] -= acc_limit
+            self.kinematics.acceleration[0] -= acc_limit
         if keys[pygame.K_RIGHT]:
-            self.acceleration[0] += acc_limit
+            self.kinematics.acceleration[0] += acc_limit
         if keys[pygame.K_UP]:
-            self.acceleration[1] -= acc_limit
+            self.kinematics.acceleration[1] -= acc_limit
         if keys[pygame.K_DOWN]:
-            self.acceleration[1] += acc_limit
+            self.kinematics.acceleration[1] += acc_limit
 
-        acc_mag = length(self.acceleration)
+        acc_mag = length(self.kinematics.acceleration)
         if acc_mag > acc_limit:
-            self.acceleration[0] = self.acceleration[0] / acc_mag * acc_limit
-            self.acceleration[1] = self.acceleration[1] / acc_mag * acc_limit
+            self.kinematics.acceleration[0] = self.kinematics.acceleration[0] / acc_mag * acc_limit
+            self.kinematics.acceleration[1] = self.kinematics.acceleration[1] / acc_mag * acc_limit
 
     def modify_speed(self):
         speed_limit = 8
-        speed_mag = length(self.speed)
+        speed_mag = length(self.kinematics.speed)
         if speed_mag > speed_limit:
-            self.speed[0] = self.speed[0] / speed_mag * speed_limit
-            self.speed[1] = self.speed[1] / speed_mag * speed_limit
+            self.kinematics.speed[0] = self.kinematics.speed[0] / speed_mag * speed_limit
+            self.kinematics.speed[1] = self.kinematics.speed[1] / speed_mag * speed_limit
 
         super().modify_speed()
 
     def move(self):
         super().move()
-        if self.position[0] < self.size:
-            self.position[0] = self.size
-        if self.position[0] > WINDOW_WIDTH - self.size:
-            self.position[0] = WINDOW_WIDTH - self.size
-        if self.position[1] < self.size:
-            self.position[1] = self.size
-        if self.position[1] > WINDOW_HEIGHT - self.size:
-            self.position[1] = WINDOW_HEIGHT - self.size
+        if self.kinematics.position[0] < self.size:
+            self.kinematics.position[0] = self.size
+        if self.kinematics.position[0] > WINDOW_WIDTH - self.size:
+            self.kinematics.position[0] = WINDOW_WIDTH - self.size
+        if self.kinematics.position[1] < self.size:
+            self.kinematics.position[1] = self.size
+        if self.kinematics.position[1] > WINDOW_HEIGHT - self.size:
+            self.kinematics.position[1] = WINDOW_HEIGHT - self.size
 
 class EnemyActor(PolygonActor):
+    """Class for all enemies"""
+
     def __init__(self):
         """Spawn in enemy from a random edge witg random speed"""
         pos = [0, 0]
         spd = [0, 0]
         size = random.randint(1, 40)
+        # choose a random edge
         edge = random.randint(0, 3)
         if edge == 0: # Left Edge
             pos = [-size, random.randint(0, WINDOW_HEIGHT)]
@@ -139,10 +158,10 @@ class EnemyActor(PolygonActor):
         rand_speed = random.randint(20, 100)/60
         spd[0] *= rand_speed
         spd[1] *= rand_speed
-        super().__init__(spd, pos, 1, size, 5)
+        super().__init__(Kinematics(spd, pos, 1, [0, 0]), size, 5)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # TODO refactor into function
     pygame.init()
 
     CLOCK = pygame.time.Clock()
@@ -151,7 +170,10 @@ if __name__ == "__main__":
 
     SCREEN = pygame.display.set_mode(WINDOW_SIZE)
 
-    PLAYER = PlayerActor([0, 0], [WINDOW_WIDTH/2, WINDOW_HEIGHT/2], 0.9, 4, 8)
+    PLAYER = PlayerActor(
+        Kinematics([0, 0], [WINDOW_WIDTH/2, WINDOW_HEIGHT/2], 0.9, [0, 0]),
+        4, 8
+    )
 
     ENEMIES = []
 
@@ -189,6 +211,8 @@ if __name__ == "__main__":
 
         CLOCK.tick(FRAMERATE)  # make sure game runs at correct framerate
 
-    while 1:
-        CLOCK.tick(FRAMERATE)
-        continue
+
+    print("Game over")
+    pygame.quit()
+    # TODO game over
+    
