b24ee8f019b7c07091df2d9cb2a96a98cfaa92f6
Gabriel Mukobi <gabrielmukobi@gmail.com>
2019-04-17 21:52:04 -0700

    refactor sprite data (geometry, coloring) into dataclass

--- Python Game/game.py
+++ Python Game/game.py
@@ -8,7 +8,7 @@ import math
 from math import sqrt, sin, cos
 import sys
 import random
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 import pygame
 
@@ -16,6 +16,7 @@ import pygame
 FRAMERATE = 60
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
+
 
 @dataclass
 class MoveData:
@@ -25,13 +26,15 @@ class MoveData:
     acceleration: List[float]
     friction: float
 
+
 @dataclass
 class SpriteData:
-    """Data class to encapsulate information about movement"""
-    speed: List[float]
-    position: List[float]
-    acceleration: List[float]
-    friction: float
+    """Data class to encapsulate information about a polygon sprite"""
+    size: int
+    num_sides: int
+    outline_color: List[int] = field(default_factory=list)
+    fill_color: List[int] = field(default_factory=list)
+    verts: List[float] = field(default_factory=list)
 
 def length(vec):
     """Get the distance between 2 2D vectors"""
@@ -70,37 +73,37 @@ class Actor:
 class PolygonActor(Actor):
     """Class for all enemies"""
 
-    def __init__(self, move_data, size, num_sides, screen):
+    def __init__(self, move_data, sprite_data, screen):
         super().__init__(move_data)
-        self.size = size
-        self.num_sides = num_sides
-        self.verts = []
-        self.outline_color = [255, 255, 255]
-        self.fill_color = [0, 0, 255]
+        self.sprite_data = sprite_data
+        self.sprite_data.outline_color = [255, 255, 255]  # default white outline
         self.screen = screen
 
     def generate_poly(self):
         """Populate the list verts with vertices of actor's polygon"""
         verts = []
-        for i in range(0, self.num_sides):
-            angle = 2.0 * math.pi / self.num_sides * i
-            vert_x = self.size * cos(angle) + self.move_data.position[0]
-            vert_y = self.size * sin(angle) + self.move_data.position[1]
+        for i in range(0, self.sprite_data.num_sides):
+            angle = 2.0 * math.pi / self.sprite_data.num_sides * i
+            vert_x = self.sprite_data.size * cos(angle) + self.move_data.position[0]
+            vert_y = self.sprite_data.size * sin(angle) + self.move_data.position[1]
             verts.append([vert_x, vert_y])
-        self.verts = verts
+        self.sprite_data.verts = verts
 
     def draw(self):
         """Generate polygon geometry then draws shape accordingly"""
         self.generate_poly()
-        pygame.draw.polygon(self.screen, self.fill_color, self.verts)
-        pygame.draw.aalines(self.screen, self.outline_color, True, self.verts)
+        pygame.draw.polygon(
+            self.screen, self.sprite_data.fill_color, self.sprite_data.verts)
+        pygame.draw.aalines(
+            self.screen, self.sprite_data.outline_color, True, self.sprite_data.verts)
 
     def collides_with(self, other_polygon_actor):
         """Test if colliding with another polygon actor"""
         x_diff = self.move_data.position[0] - other_polygon_actor.move_data.position[0]
         y_diff = self.move_data.position[1] - other_polygon_actor.move_data.position[1]
         dist = sqrt(x_diff*x_diff + y_diff*y_diff)
-        return dist < self.size + other_polygon_actor.size
+        return dist < self.sprite_data.size + other_polygon_actor.sprite_data.size
+
 
 class PlayerActor(PolygonActor):
     """Class for player character"""
@@ -134,14 +137,15 @@ class PlayerActor(PolygonActor):
 
     def move(self):
         super().move()
-        if self.move_data.position[0] < self.size:
-            self.move_data.position[0] = self.size
-        if self.move_data.position[0] > WINDOW_WIDTH - self.size:
-            self.move_data.position[0] = WINDOW_WIDTH - self.size
-        if self.move_data.position[1] < self.size:
-            self.move_data.position[1] = self.size
-        if self.move_data.position[1] > WINDOW_HEIGHT - self.size:
-            self.move_data.position[1] = WINDOW_HEIGHT - self.size
+        if self.move_data.position[0] < self.sprite_data.size:
+            self.move_data.position[0] = self.sprite_data.size
+        if self.move_data.position[0] > WINDOW_WIDTH - self.sprite_data.size:
+            self.move_data.position[0] = WINDOW_WIDTH - self.sprite_data.size
+        if self.move_data.position[1] < self.sprite_data.size:
+            self.move_data.position[1] = self.sprite_data.size
+        if self.move_data.position[1] > WINDOW_HEIGHT - self.sprite_data.size:
+            self.move_data.position[1] = WINDOW_HEIGHT - self.sprite_data.size
+
 
 class EnemyActor(PolygonActor):
     """Class for all enemies"""
@@ -169,13 +173,17 @@ class EnemyActor(PolygonActor):
         rand_speed = random.randint(20, 100)/60
         spd[0] *= rand_speed
         spd[1] *= rand_speed
-        super().__init__(MoveData(spd, pos, [0.0, 0.0], 1), size, 5, screen)
+        super().__init__(
+            MoveData(spd, pos, [0.0, 0.0], 1), 
+            SpriteData(size, 5, fill_color=[0, 0, 0]), screen
+        )
         self.player_ref = player_ref
         self.update_color()
 
     def update_color(self):
         """Updates the enemy color based on its size relative to the player"""
         #size_dif = self.player_ref.move_data.
+        
 
 
 def main():
@@ -191,7 +199,7 @@ def main():
 
     player = PlayerActor(
         MoveData([0, 0], [WINDOW_WIDTH/2, WINDOW_HEIGHT/2], [0, 0], 0.9),
-        4, 8, screen
+        SpriteData(4, 8, fill_color=[0, 178, 238]), screen
     )
 
     enemies = []
@@ -217,8 +225,8 @@ def main():
         non_colliding_enemies = []
         for enemy in enemies:
             if player.collides_with(enemy):
-                if player.size > enemy.size:
-                    player.size += 1
+                if player.sprite_data.size > enemy.sprite_data.size:
+                    player.sprite_data.size += 1
                 else:
                     game_is_playing = False
             else:
